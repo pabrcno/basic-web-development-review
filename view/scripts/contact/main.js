00d602fa93d2fb89713856dfbdda3dea0a026811
pabrcno <pabrcn@gmail.com>
2021-11-22 21:52:45 -0300

    ajax xall

--- view/scripts/contact/main.js
+++ view/scripts/contact/main.js
@@ -16,5 +16,21 @@ $("#message").on("change", function () {
 });
 //on send click print data
 $("#send").click(function () {
-  alert(data.name + "\n " + data.email + "\n " + data.message + "\n" + "Sent");
+  ajaxCall();
 });
+
+//AJAX call to http://numbersapi.com/number/math with a random number
+//and print the response
+
+ajaxCall = () => {
+  $.ajax({
+    url: "http://numbersapi.com/random/math",
+    success: function (response) {
+      alert(
+        data.name + "\n " + data.email + "\n " + data.message + "\n" + "Sent"
+      );
+      console.log(response);
+    },
+  });
+  console.log("ajax call");
+};
